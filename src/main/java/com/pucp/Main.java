14f912fc0ed9b164b046bfc0affbd4158dc6d82e
Gregory Valderrama <gvalderrama@terremark.com>
2015-09-28 21:02:47 -0500

    add

--- src/main/java/com/pucp/Main.java
+++ src/main/java/com/pucp/Main.java
@@ -3,12 +3,16 @@ import com.hp.hpl.jena.ontology.OntModel;
 import com.hp.hpl.jena.ontology.OntModelSpec;
 import com.hp.hpl.jena.query.*;
 import com.hp.hpl.jena.rdf.model.*;
+import com.hp.hpl.jena.reasoner.Derivation;
 import com.hp.hpl.jena.reasoner.ValidityReport;
+import com.hp.hpl.jena.reasoner.rulesys.GenericRuleReasoner;
 import com.hp.hpl.jena.reasoner.rulesys.GenericRuleReasonerFactory;
+import com.hp.hpl.jena.reasoner.rulesys.Rule;
 import com.hp.hpl.jena.vocabulary.*;
 import com.hp.hpl.jena.util.FileManager;
 import java.io.*;
 import java.util.Iterator;
+import java.util.List;
 
 import com.hp.hpl.jena.rdf.model.InfModel;
 import com.hp.hpl.jena.rdf.model.Model;
@@ -23,18 +27,169 @@ import com.hp.hpl.jena.util.FileManager;
 import com.hp.hpl.jena.util.PrintUtil;
 import com.hp.hpl.jena.vocabulary.ReasonerVocabulary;
 
+import javax.swing.*;
+
 public class Main {
     public static String RecetarioXS = "http://www.recetario.com/";
     static final String inputFileName  = "receta_base.owl";
 
-    public static void main(String[] args) {
-	// write your code here
+
+    private static void PrintModel(Model model)
+    {
+        System.out.println("Printing Model========");
+        StmtIterator iter = model.listStatements();
+
+        // print out the predicate, subject and object of each statement
+        while (iter.hasNext()) {
+            Statement stmt      = iter.nextStatement();  // get next statement
+            Resource  subject   = stmt.getSubject();     // get the subject
+            Property  predicate = stmt.getPredicate();   // get the predicate
+            RDFNode   object    = stmt.getObject();      // get the object
+
+            System.out.print(subject.toString());
+            System.out.print(" " + predicate.toString() + " ");
+            if (object instanceof Resource) {
+                System.out.print(object.toString());
+            } else {
+                // object is a literal
+                System.out.print(" \"" + object.toString() + "\"");
+            }
+            System.out.println(" .");
+        }
+    }
+    private  static void PrintResource(Resource resource)
+    {
+        StmtIterator iter = resource.listProperties();
+        while (iter.hasNext()) {
+            System.out.println("    " + iter.nextStatement()
+                    .getObject()
+                    .toString());
+        }
+    }
+
+    private static  void RecetarioSample()
+    {
+        Model instances = ModelFactory.createDefaultModel();
+        instances.read("file:recetas_turtle.ttl");
+        //instances.read("file:receta_base.owl");
+       // Reasoner reasoner = new
+         //       GenericRuleReasoner(Rule.rulesFromURL("file:rulesa.rdf"));
+        //reasoner.setDerivationLogging(true);
+        //InfModel inf = ModelFactory.createInfModel(reasoner, instances);
+
+
+        Model rule_model = ModelFactory.createDefaultModel();
+        Resource configuration = rule_model.createResource();
+        configuration.addProperty(ReasonerVocabulary.PROPruleSet, "rulesa.rdf");
+        Reasoner reasoner = GenericRuleReasonerFactory.theInstance().create(configuration);
+        InfModel inf = ModelFactory.createInfModel(reasoner, instances);
+
+
+        Resource A = inf.getResource("http://www.w3.org/2002/07/owl#Receta");
+        Resource D = inf.getResource("http://example.org/D");
+
+        Resource Tallarines = inf.getResource("http://www.w3.org/2002/07/owl#Tallarines");
+
+        Resource tiene_ingrediente = inf.getResource("http://www.w3.org/2002/07/owl#tiene_ingrediente");
+
+        Property subClass = inf.getProperty("http://www.w3.org/2000/01/rdf-schema#subClassOf");
+        Property p = inf.getProperty("http://www.recetario.com/nivelCalorias");
+        Property pp = inf.getProperty("http://www.recetario.com/tiene");
+        Property type_of = inf.getProperty("http://www.w3.org/1999/02/22-rdf-syntax-ns#type");
+        Property on_property = inf.getProperty("http://www.w3.org/2002/07/owl#onProperty");
+
+
+
+
+        StmtIterator iter = inf.listStatements(Tallarines, null, (RDFNode) null );
+        //StmtIterator iter = inf.listStatements();
+        while (iter.hasNext()) {
+            Statement stmt      = iter.nextStatement();
+            Resource  subject   = stmt.getSubject();
+            Property  predicate = stmt.getPredicate();
+            RDFNode   object    = stmt.getObject();
+
+            System.out.print(subject.toString());
+            System.out.print(" " + predicate.toString() + " ");
+            if (object instanceof Resource) {
+                System.out.print(object.toString());
+            } else {
+                // object is a literal
+                System.out.print(" \"" + object.toString() + "\"");
+            }
+            System.out.println(" .");
+        }
+    }
+
+    private static void SimpleDemo()
+    {
+        Model instances = ModelFactory.createDefaultModel();
+        instances.read("file:test.ttl");
+        Reasoner reasoner = new
+                GenericRuleReasoner(Rule.rulesFromURL("file:simple_rules.rdf"));
+
+        reasoner.setDerivationLogging(true);
+        InfModel inf = ModelFactory.createInfModel(reasoner, instances);
+
+        PrintModel(inf);
+
+        /*
+        StmtIterator iter = inf.listStatements();
+        while (iter.hasNext()) {
+            Statement stmt      = iter.nextStatement();
+            Resource  subject   = stmt.getSubject();
+            Property  predicate = stmt.getPredicate();
+            RDFNode   object    = stmt.getObject();
+
+            System.out.print(subject.toString());
+            System.out.print(" " + predicate.toString() + " ");
+            if (object instanceof Resource) {
+                System.out.print(object.toString());
+            } else {
+                // object is a literal
+                System.out.print(" \"" + object.toString() + "\"");
+            }
+            System.out.println(" .");
+        }
+        */
+/*
+
+        Model raw_model = FileManager.get().loadModel("test.ttl");
+        //String rules = "[rule1: (?a eg:p ?b) (?b eg:p ?c) -> (?a eg:p ?c)]";
+        List<Rule> rules =  Rule.rulesFromURL("simple_rules.rdf");
+        Reasoner reasoner = new GenericRuleReasoner(rules);
+        reasoner.setDerivationLogging(true);
+        InfModel inf = ModelFactory.createInfModel(reasoner, raw_model);
+
+        Resource A = inf.getResource("http://example.org/A");
+        Resource D = inf.getResource("http://example.org/D");
+        Property p = inf.getProperty("http://example.org/p");
+
+        PrintWriter out = new PrintWriter(System.out);
+
+        for (StmtIterator i = inf.listStatements((Resource)null,p,D); i.hasNext(); ) {
+            Statement s = i.nextStatement();
+            System.out.println("Statement is " + s);
+            for (Iterator id = inf.getDerivation(s); id.hasNext(); ) {
+                Derivation deriv = (Derivation) id.next();
+                deriv.printTrace(out, true);
+            }
+        }
+        out.flush();
+*/
+    }
+
+    private static void Recetario()
+    {
+        // write your code here
 
         //OntModel ont_model =  ModelFactory.createOntologyModel( OntModelSpec.OWL_MEM );
         Model raw_model = FileManager.get().loadModel("receta_base.owl");
 
+        //PrintModel(raw_model);
+
         InfModel inf_model =  ModelFactory.createRDFSModel(raw_model);
-      //  FileManager.get().readModel( ont_model, inputFileName );
+        //  FileManager.get().readModel( ont_model, inputFileName );
 
 
         ValidityReport validaty = inf_model.validate();
@@ -47,50 +202,69 @@ public class Main {
         }
 
 
-        Resource resource =  inf_model.getResource("http://www.recetario.com/Vegetariana");
+        Model rule_model = ModelFactory.createDefaultModel();
+        Resource configuration = rule_model.createResource();
+        List<Rule> rules =  Rule.rulesFromURL("rulesa.rdf");
+        //String rules = "[rule1: (?a eg:p ?b) (?b eg:p ?c) -> (?a eg:p ?c)]";
+        Reasoner reasoner = new GenericRuleReasoner(rules);
 
-        StmtIterator iter = resource.listProperties();
-        while (iter.hasNext()) {
-            System.out.println("    " + iter.nextStatement()
-                    .getObject()
-                    .toString());
+
+
+        reasoner.setDerivationLogging(true);
+        InfModel inf = ModelFactory.createInfModel(reasoner, raw_model);
+
+        PrintModel(inf.getDeductionsModel());
+
+        /*
+        PrintWriter out = new PrintWriter(System.out);
+        Resource receta = inf.getResource("http://www.recetario.com/AltasCalorias");
+
+        for (StmtIterator i = inf.listStatements( receta, null, (Resource)null) ; i.hasNext(); ) {
+            Statement s = i.nextStatement();
+            System.out.println("Statement is " + s);
+            for (Iterator id = inf.getDerivation(s); id.hasNext(); ) {
+                Derivation deriv = (Derivation) id.next();
+                deriv.printTrace(out, true);
+            }
         }
+        out.flush();
 
-
+        */
+        /*
+        Resource resource =  inf_model.getResource("http://www.recetario.com/AltasCalorias");
+        PrintResource(resource);
 
         Model rule_model = ModelFactory.createDefaultModel();
         Resource configuration = rule_model.createResource();
-        configuration.addProperty(ReasonerVocabulary.PROPruleSet, "rules.rdf");
+        configuration.addProperty(ReasonerVocabulary.PROPruleSet, "rulesa.rdf");
         Reasoner reasoner = GenericRuleReasonerFactory.theInstance().create(configuration);
+        InfModel reasoner_model = ModelFactory.createInfModel(reasoner, inf_model);
 
-        InfModel rule_inf_model = ModelFactory.createInfModel(reasoner, inf_model);
+
+        Model deduction = reasoner_model.getDeductionsModel();
+        PrintModel(deduction);
+        */
 
 
-        PrintUtil.registerPrefix("recetario", "http://www.recetario.com/#");
-        for (StmtIterator i = rule_inf_model.listStatements(resource, null,(Resource)null); i.hasNext(); ) {
-             Statement stmt = i.nextStatement();
-             System.out.println(" - " + PrintUtil.print(stmt));
+        /*
+        for (StmtIterator i = reasoner_model.listStatements((Resource)null , null,resource); i.hasNext(); ) {
+            Statement stmt = i.nextStatement();
+            System.out.println(" rs -- " + PrintUtil.print(stmt));
         }
-
+        */
+      /*
+        PrintUtil.registerPrefix("recetario", "http://www.recetario.com/#");
+        */
         //https://jena.apache.org/tutorials/rdf_api.html
         //https://github.com/gcvalderrama/pucp_km_jena/blob/master/src/main/java/com/pucp/Main.java
         //https://jena.apache.org/tutorials/rdf_api.html
-
-
-
-
         //  Model model = ModelFactory.createDefaultModel();
-
         // InputStream in = FileManager.get().open( inputFileName );
-
         //  if (in == null) {
         //      throw new IllegalArgumentException( "File: " + inputFileName + " not found");
         //  }
-
         // read the RDF/XML file
         // model.read(new InputStreamReader(in), "");
-
-
         // write it to standard out
         //ont_model.write(System.out);
         /*
@@ -114,6 +288,8 @@ public class Main {
         }
         */
 
-        System.out.println("hola mundo");
+    }
+    public static void main(String[] args) {
+        RecetarioSample();
     }
 }
